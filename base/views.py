ab18a3d128fc6e3352c830bd2b5806a37c0a8a69
David-Chan-Ho2 <dho12@charlotte.edu>
2025-01-29 16:53:29 -0500

    add templating and urls

--- base/views.py
+++ base/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
 
-def room(request):
-    return render(request, 'room.html')
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Lets learn python!'},
+    {'id': 3, 'name': 'Lets learn python!'},
+]
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
