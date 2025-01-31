75b98cb79e1ecf9d31c476d7621e1e5efb174638
David-Chan-Ho2 <dho12@charlotte.edu>
2025-01-31 14:10:51 -0500

    flash messages

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic, User
 from .forms import RoomForm
 
@@ -9,8 +12,18 @@ def loginPage(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         
-        # try:
-        user = User.objects.get(username=username)
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does exist')
         
     context = {}
     return render(request, 'base/login_register.html', context)
